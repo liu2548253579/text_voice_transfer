4b9449e3936e786e13ccc668c88871c4056ac56d
Frank Liu <2548253579@qq.com>
2023-04-19 00:45:57 +0800

    update

--- baidu_ai.py
+++ baidu_ai.py
@@ -1,5 +1,9 @@
 from aip import AipSpeech
 
+APP_ID=None
+API_KEY=None
+SECRET_KEY=None
+
 #从baidu_api.txt中读取APP_ID, API_KEY, SECRET_KEY,格式为APP_ID:xxxxx 以此类推
 with open('baidu_api.txt', 'r') as f:
     lines = f.readlines()
@@ -21,10 +25,10 @@ def audio_to_text(wav_file):
 def text_to_audio(synth_file, res_str):
     # 准备语音合成
     synth_context = client.synthesis(res_str,'zh',1,{
-        'spd' : 4,  #语速(0-9)
-        'vol' : 9,  #音量(0-9)
+        'spd' : 5,  #语速(0-9)
+        'vol' : 5,  #音量(0-9)
         'pit' : 5,  #音调(0-9)
-        'per' : 2,  #发音人·0：女声，1：男声，3：情感合成-度逍遥，4：情感合成-度丫丫
+        'per' : 4,  #发音人·0：女声，1：男声，3：情感合成-度逍遥，4：情感合成-度丫丫
     })
 
     # 确定合成内容已生成，因为生成错误会返回字典类型报错
